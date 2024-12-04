2c779df03d296089768535ef652e676685067ebe
ericthayer <ethayer.design@gmail.com>
2024-12-04 16:17:42 -0700

    Test rolllup config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,26 @@ export default {
   root: path.resolve(__dirname, 'src'),
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+              return 'assets/images/[name]-[hash][extname]';
+          }
+          
+          if (/\.css$/.test(name ?? '')) {
+              return 'assets/css/[name]-[hash][extname]';   
+          }
+ 
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    }
   },
   server: {
     port: 8080
