f592b3252cebe2af05021b921f73b3453b3e2537
ericthayer <ethayer.design@gmail.com>
2024-12-04 23:42:53 -0700

    Update lighthouse audit dist build

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,42 @@
-import { join } from "node:path";
-import { buildSync } from "esbuild";
+import { join } from "node:path"
+import { buildSync } from "esbuild"
+import { defineConfig } from "vite"
 
-const path = require('path')
+const path = require("path")
 
-export default {
-  root: path.resolve(__dirname, 'src'),
+export default defineConfig({
+  root: path.resolve(__dirname, "src"),
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "src/index.html"),
+        nested: path.resolve(__dirname, "src/lighthouse/index.html"),
+      },
       output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        
-        assetFileNames: ({name}) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
-              return 'assets/images/[name]-[hash][extname]';
-          }
-          
-          if (/\.css$/.test(name ?? '')) {
-              return 'assets/css/[name]-[hash][extname]';   
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]"
           }
 
-          if (/\.js$/.test(name ?? '')) {
-            return 'assets/js/[name][extname]';   
-        }
- 
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]"
+          }
+
           // default value
           // ref: https://rollupjs.org/guide/en/#outputassetfilenames
-          return 'assets/[name]-[hash][extname]';
+          return "assets/[name]-[hash][extname]"
         },
       },
-    }
+    },
   },
   server: {
-    port: 8080
+    port: 8080,
   },
   plugins: [
-    // ...
     {
       apply: "build",
       enforce: "post",
@@ -46,8 +46,8 @@ export default {
           bundle: true,
           entryPoints: [join(process.cwd(), "service-worker.js")],
           outfile: join(process.cwd(), "dist", "service-worker.js"),
-        });
+        })
       },
     },
   ],
-}
+})
