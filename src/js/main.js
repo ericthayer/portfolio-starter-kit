f64a449923897c78f33fb4648bb496d7e24b6c43
ericthayer <ethayer.design@gmail.com>
2025-02-18 21:00:42 -0700

    WIP resume formatting

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,26 @@ import * as bootstrap from "bootstrap"
 // BS color picker
 import "./bs-color-picker.js"
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+)
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+)
+
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+}
+
+const fadeInRoot = document.querySelector(".enhanced")
+fadeInRoot.classList.add("show")
+
+if (isMobile()) {
+  document.querySelector(".bd-mode-toggle").classList.add("d-none")
+  document.querySelector(".brand").classList.add("ms-1")
+}
 
 import { register } from "register-service-worker"
 
